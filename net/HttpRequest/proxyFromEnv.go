12e81c7b28733f01942e0fa4de00a78839be886e
张耀耀 <yy.zhang@9-net.cn>
2022-07-23 09:50:11 +0800

    update

--- net/HttpRequest/proxyFromEnv.go
+++ net/HttpRequest/proxyFromEnv.go
@@ -13,13 +13,11 @@ import (
 
 func main() {
 	// 如果访问的是 https 就要用 https_proxy 环境变量
-	//os.Setenv("HTTPS_PROXY", "http://127.0.0.1:7890")
-	os.Setenv("HTTP_PROXY", "http://127.0.0.1:7890")
+	os.Setenv("HTTPS_PROXY", "http://127.0.0.1:7890")
+	//os.Setenv("HTTP_PROXY", "http://127.0.0.1:7890")
 	req := HttpRequest.NewRequest().SetTimeout(30).Debug(false)
 
-	resp, err := req.Get("http://192.168.1.91:9971/t01", map[string]interface{}{
-		"id": "eq.1",
-	})
+	resp, err := req.Get("https://dapi.binance.com/dapi/v1/time")
 	defer resp.Close()
 
 	if err != nil {
